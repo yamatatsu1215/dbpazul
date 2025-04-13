7bf07f04edf5a58123ef4c919868d655f623f0dc
yamatatsu1215 <tatsuyama3140@gmail.com>
2025-04-13 21:47:35 +0900

    ルーム作成機能にユーザー認証を追加し、作成者情報を保存するように変更

--- src/app/api/room/route.ts
+++ src/app/api/room/route.ts
@@ -2,6 +2,7 @@
 import { NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabase';
 import { Room } from '@/types/types';
+import { cookies } from 'next/headers';
 
 // Roomテーブルの全件取得
 export async function GET() {
@@ -19,9 +20,28 @@ export async function GET() {
 export async function POST(request: Request) {
     const { name } = await request.json();
 
+    const cookie = await cookies();
+    const token = cookie.get('auth_token')?.value;
+
+    if (!token) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    // トークンを使用してユーザー情報を取得する例
+    const { data: userData, error: userError } = await supabase.auth.getUser(token);
+    if (userError) {
+        return NextResponse.json({ error: userError.message }, { status: 401 });
+    }
+    const user = userData.user;
+    if (!user) {
+        return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const userId = user.id;
+
     const { data, error } = await supabase.from('room').insert([
         {
             name: name,
+            createdBy: userId,
         },
     ]);
 
