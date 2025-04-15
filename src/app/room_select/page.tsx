b1a2fd4dc7aa6b6e89e7014ad7cc16324fde817b
yamatatsu1215 <tatsuyama3140@gmail.com>
2025-04-15 23:03:41 +0900

    コードのインデントを修正し、可読性を向上させるために整形

--- src/app/room_select/page.tsx
+++ src/app/room_select/page.tsx
@@ -4,38 +4,40 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const RoomSelectPage = () => {
-    const [room, setRoom] = useState<Room[]>([]);
+  const [room, setRoom] = useState<Room[]>([]);
 
-    useEffect(() => {
-        async function fetchRooms() {
-            const response = await fetch("/api/room",{
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            if (response.ok) {
-                const data = await response.json();
-                setRoom(data);
-            }
-        }
-            fetchRooms();
-    },[]);
-    return (
-        <div>
-            <h1>ルーム一覧</h1>
-            <ul>
-                {room.map((r) => (
-                    <li key={r.id}>
-                        <h2>{r.name}</h2>
-                        <p>ルームID: {r.id}</p>
-                        <p>作成日時: {new Date(r.createdAt).toLocaleString()}</p>
-                    </li>
-                ))}
-            </ul>
-            <Link href="room_create">ルーム作成</Link>
-        </div>
-    );
+  useEffect(() => {
+    async function fetchRooms() {
+      const response = await fetch("/api/room", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setRoom(data);
+      }
+    }
+    fetchRooms();
+  }, []);
+  return (
+    <div>
+      <h1>ルーム一覧</h1>
+      <ul>
+        {room.map((r) => (
+          <li key={r.id}>
+            <Link href={`/room_select/${r.id}`}>
+              <h2>{r.name}</h2>
+              <p>ルームID: {r.id}</p>
+              <p>作成日時: {new Date(r.createdAt).toLocaleString()}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Link href="room_create">ルーム作成</Link>
+    </div>
+  );
 };
 
 export default RoomSelectPage;
